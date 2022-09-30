1eb100fb7a912afc0c392019faa5380749bd758c
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2022-09-30 13:15:30 +0700

    add dark mode and make pop up profile picture

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,49 @@ const certificateContainer = document.querySelector("#certificate");
 const skillsContainer = document.querySelector("#skills");
 const experienceContainer = document.querySelector("#experience");
 const projectContainer = document.querySelector("#project");
+const darkToogle = document.querySelector("#dark-toogle");
+const headProfile = document.querySelector("#head-profile");
+const contactLogo = document.querySelector("#contact-logo");
+const buttonContainer = document.querySelector("#button-container");
+const toogleSection = document.querySelector("#toogle-section");
+const bodyHtml = document.querySelector(".body");
+const footerHtml = document.querySelector(".footer");
+const particlesLight = document.querySelector("#particles-js");
+const particlesDark = document.querySelector("#particles-dark");
+const profilePicture = document.querySelector("#profile-picture");
+
+// Pop up image
+profilePicture.addEventListener("click", function () {
+  Swal.fire({
+    imageUrl: "../assets/image/my-profile.png",
+    imageHeight: 500,
+    imageAlt: "A tall image",
+    showConfirmButton: false,
+  });
+});
+
+// Dark Mode
+darkToogle.addEventListener("click", function () {
+  if (darkToogle.checked) {
+    bodyHtml.classList.add("dark");
+    headProfile.classList.add("dark");
+    contactLogo.classList.add("dark");
+    buttonContainer.classList.add("dark");
+    toogleSection.classList.add("dark");
+    footerHtml.classList.add("dark");
+    particlesDark.classList.remove("hidden");
+    particlesLight.classList.add("hidden");
+  } else {
+    bodyHtml.classList.remove("dark");
+    headProfile.classList.remove("dark");
+    contactLogo.classList.remove("dark");
+    buttonContainer.classList.remove("dark");
+    toogleSection.classList.remove("dark");
+    footerHtml.classList.remove("dark");
+    particlesDark.classList.add("hidden");
+    particlesLight.classList.remove("hidden");
+  }
+});
 
 buttonSkills.addEventListener("click", function () {
   certificateContainer.classList.add("hidden");
